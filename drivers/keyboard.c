29d5065f2c0829ef68d4b705036eebe1f8b995af
Pengwius <tomekftomekf@gmail.com>
2021-07-18 19:31:13 +0200

    Add capslock implementation

--- drivers/keyboard.c
+++ drivers/keyboard.c
@@ -6,19 +6,37 @@
 #include "../libc/function.h"
 #include "../kernel/kernel.h"
 
+typedef enum
+{
+    true=1, false=0
+}bool;
+
 #define BACKSPACE 0x0E
 #define ENTER 0x1C
+#define LSHIFT 0x2A
+#define CAPSLOCK 0x3A
 
 static char key_buffer[256];
 
-#define SC_MAX 57
+bool capslock_toogle = false;
+
+int toUpper(int ch)
+{
+    if(ch>='a' && ch<='z')
+    return('A' + ch - 'a');
+    else
+    return(ch);
+}
+
+#define SC_MAX 58
 const char *sc_name[] = { "ERROR", "Esc", "1", "2", "3", "4", "5", "6", 
     "7", "8", "9", "0", "-", "=", "Backspace", "Tab", "Q", "W", "E", 
         "R", "T", "Y", "U", "I", "O", "P", "[", "]", "Enter", "Lctrl", 
         "A", "S", "D", "F", "G", "H", "J", "K", "L", ";", "'", "`", 
         "LShift", "\\", "Z", "X", "C", "V", "B", "N", "M", ",", ".",
-        "/", "RShift", "Keypad *", "LAlt", "Spacebar"};
-const char sc_ascii[] = { 'E', '?', '1', '2', '3', '4', '5', '6',     
+        "/", "RShift", "Keypad *", "LAlt", "Spacebar", "Capslock"};
+
+const char sc_ascii[] = { '?', '?', '1', '2', '3', '4', '5', '6',     
     '7', '8', '9', '0', '-', '=', '?', '?', 'q', 'w', 'e', 'r', 't', 'y', 
         'u', 'i', 'o', 'p', '[', ']', '?', '?', 'a', 's', 'd', 'f', 'g', 
         'h', 'j', 'k', 'l', ';', '\'', '`', '?', '\\', 'z', 'x', 'c', 'v', 
@@ -36,12 +54,24 @@ static void keyboard_callback(registers_t regs) {
         kprint("\n");
         user_input(key_buffer); /* kernel-controlled function */
         key_buffer[0] = '\0';
+    } else if (scancode == CAPSLOCK) {
+        if (capslock_toogle == true) {
+            capslock_toogle = false;
+        } else {
+            capslock_toogle = true;
+        }
     } else {
         char letter = sc_ascii[(int)scancode];
         /* Remember that kprint only accepts char[] */
+        
+        if (capslock_toogle == true) {
+            letter = toUpper(letter);
+        }
+
         char str[2] = {letter, '\0'};
         append(key_buffer, letter);
         kprint(str);
+        
     }
     UNUSED(regs);
 }
