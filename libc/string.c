c911b29fbe955c5bdccb5d8c556ac1ef6b9d4226
Tomasz F <tomekftomekf@gmail.com>
2021-12-10 17:06:37 +0100

    File system (#5)
    * Users command - getting type of operation from query
    
    * Fix "command not found" on "user" command
    
    * Getting parameters from command
    
    * Simple error handling
    
    * Getting values of attributes from command
    
    * Getting value of attribute
    
    * Giving NewUser object to extern function
    
    * Fix one-letter-accepted-commands

--- libc/string.c
+++ libc/string.c
@@ -112,3 +112,29 @@ char* strcpy(char* destination, const char* source)
     // the destination is returned by standard `strcpy()`
     return ptr;
 }
+
+char* strcpy(char* destination, const char* source)
+{
+    // return if no memory is allocated to the destination
+    if (destination == NULL) {
+        return NULL;
+    }
+ 
+    // take a pointer pointing to the beginning of the destination string
+    char *ptr = destination;
+ 
+    // copy the C-string pointed by source into the array
+    // pointed by destination
+    while (*source != '\0')
+    {
+        *destination = *source;
+        destination++;
+        source++;
+    }
+ 
+    // include the terminating null character
+    *destination = '\0';
+ 
+    // the destination is returned by standard `strcpy()`
+    return ptr;
+}
